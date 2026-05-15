e02cd2fa253c2601ac20366f869d3b2f22a552ae
pengguanzhen <Gavin.Peng@lango-tech.com>
2026-05-15 18:58:33 +0800

    chore: 升级版本号到2.3.2
    - 更新APP_VERSION为2.3.2
    - 版本配置文件同步更新

--- adb_gui.py
+++ adb_gui.py
@@ -417,7 +417,7 @@ class ADBGUI(QMainWindow):
     device_info_updated = pyqtSignal(str, dict)  # 设备信息更新信号
 
     # 版本号
-    APP_VERSION = "2.003.016"
+    APP_VERSION = "2.3.2"
 
     def __init__(self):
         super().__init__()
